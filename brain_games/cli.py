185908550ba515657208b00366e6dc081a6c8729
Alexander Potapkov <xanben@mail.ru>
2022-01-18 23:07:32 +0300

    add CodeClimate, flake8 and Github Actions

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,10 @@
+"""This script starts the Brain Games program."""
+
 import prompt
 
 
 def welcome_user():
+    """Return a welcome message."""
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
-    print('Hello, {}!'.format(name))
+    print('Hello, {0}!'.format(name))
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
+
+"""This script calls the function welcome_user() from cli.py."""
+
 from brain_games import cli
 
+
 def main():
+    """Return func welcome_user."""
     cli.welcome_user()
+
 
 if __name__ == '__main__':
     main()
