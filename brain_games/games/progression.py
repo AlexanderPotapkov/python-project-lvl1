86eec0cd4069e682a65f087a03cc445af7b5c81b
Alexander Potapkov <xanben@mail.ru>
2022-02-02 21:26:23 +0300

    brain_games/games/progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,19 +3,29 @@
 from random import choice, randint
 
 GAME_RULE = 'What number is missing in the progression?'
+MIN_TERM_OF_ARITHMETIC_PROGRESSION = 1
+MAX_TERM_OF_ARITHMETIC_PROGRESSION = 100
+MIN_DIFFERENCE = 1
+MAX_DIFFERENCE = 10
+
+
+def get_arithmetic_progression(term_of_progression, difference):
+    progression = list(range(1, term_of_progression, difference))
+    answer = choice(progression)
+    for index, sequence in enumerate(progression):
+        if sequence == answer:
+            progression[index] = '..'
+        else:
+            progression[index] = str(sequence)
+    return progression, answer
 
 
 def get_question_and_solution():
-    start = randint(1, 10)
-    stop = randint(60, 100)
-    step = randint(1, 6)
-    question = list(range(start, stop, step))
-
-    correct_answer = choice(question)
-    for index, sequence in enumerate(question):
-        if sequence == correct_answer:
-            question[index] = '..'
-        else:
-            question[index] = str(sequence)
-
+    term_of_progression = randint(
+        MIN_TERM_OF_ARITHMETIC_PROGRESSION,
+        MAX_TERM_OF_ARITHMETIC_PROGRESSION)
+    difference = randint(MIN_DIFFERENCE, MAX_DIFFERENCE)
+    question, correct_answer = get_arithmetic_progression(
+        term_of_progression,
+        difference)
     return ' '.join(question), str(correct_answer)
