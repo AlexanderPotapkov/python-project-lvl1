4ee5232cf103192cb82075c35bab7fab87af7826
Alexander Potapkov <xanben@mail.ru>
2022-02-05 10:54:15 +0300

    fix brain_games/games/prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -3,20 +3,21 @@
 from random import randint
 
 GAME_RULE = 'Answer "yes" if gives number is prime. Otherwise answer "no".'
+MIN_NUMBER_FOR_QUESTION = 1
+MAX_NUMBER_FOR_QUESTION = 100
+FIRST_PRIME_NUMBER = 2
 
-first_prime_number = 2
 
-
-def is_prime(question):
-    if question < first_prime_number:
+def is_prime(number):
+    if number < FIRST_PRIME_NUMBER:
         return False
-    for i in range(first_prime_number, question // 2 + 1):
-        if question % i == 0:
+    for i in range(FIRST_PRIME_NUMBER, number // 2 + 1):
+        if number % i == 0:
             return False
     return True
 
 
 def get_question_and_solution():
-    question = randint(1, 100)
-    correct_answer = 'yes' if is_prime(question) else 'no'
-    return str(question), str(correct_answer)
+    number = randint(MIN_NUMBER_FOR_QUESTION, MAX_NUMBER_FOR_QUESTION)
+    correct_answer = 'yes' if is_prime(number) else 'no'
+    return str(number), str(correct_answer)
