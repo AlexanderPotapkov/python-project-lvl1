83a0dd8a1d9e9d0a6b89bd6483b7549408dce972
Alexander Potapkov <xanben@mail.ru>
2022-02-08 20:03:31 +0300

    fix brain_games/games/progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,48 +3,42 @@
 from random import choice, randint
 
 GAME_RULE = 'What number is missing in the progression?'
-MIN_FIRST_TERM_OF_ARITHMETIC_PROGRESSION = 1
-MAX_FIRST_TERM_OF_ARITHMETIC_PROGRESSION = 5
-MIN_TERM_OF_ARITHMETIC_PROGRESSION = 50
-MAX_TERM_OF_ARITHMETIC_PROGRESSION = 100
+MIN_TERM_OF_ARITHMETIC_PROGRESSION = 1
+MAX_TERM_OF_ARITHMETIC_PROGRESSION = 20
 MIN_DIFFERENCE = 1
-MAX_DIFFERENCE = 5
-MIN_PROGRESSION_LENGTH = 5
-MAX_PROGRESSION_LENGTH = 10
+MAX_DIFFERENCE = 10
+LENGTH_OF_PROGRESSION = 10
 
 
 def get_arithmetic_progression(
-        first_term_of_arithmetic_progression,
         initial_term,
-        common_difference,
-        progression_length):
-    progression = list(range(
-        first_term_of_arithmetic_progression,
-        initial_term, common_difference))[:progression_length]
+        common_difference):
+    term_of_progression, progression = initial_term, [initial_term]
+    for _ in range(LENGTH_OF_PROGRESSION):
+        term_of_progression += common_difference
+        progression.append(term_of_progression)
     return progression
 
 
+def stringify_progression(progression, hidden_term):
+    for index, sequence in enumerate(progression):
+        if sequence == hidden_term:
+            progression[index] = '..'
+        else:
+            progression[index] = str(sequence)
+    return ' '.join(progression)
+
+
 def get_question_and_solution():
-    first_term_of_arithmetic_progression = randint(
-        MIN_FIRST_TERM_OF_ARITHMETIC_PROGRESSION,
-        MAX_FIRST_TERM_OF_ARITHMETIC_PROGRESSION)
     initial_term = randint(
         MIN_TERM_OF_ARITHMETIC_PROGRESSION,
         MAX_TERM_OF_ARITHMETIC_PROGRESSION)
     common_difference = randint(MIN_DIFFERENCE, MAX_DIFFERENCE)
-    progression_length = randint(
-        MIN_PROGRESSION_LENGTH,
-        MAX_PROGRESSION_LENGTH)
     progression = get_arithmetic_progression(
-        first_term_of_arithmetic_progression,
         initial_term,
-        common_difference,
-        progression_length)
-    hidden_term = choice(progression)
-    question = progression
-    for index, sequence in enumerate(question):
-        if sequence == hidden_term:
-            question[index] = '..'
-        else:
-            question[index] = str(sequence)
-    return ' '.join(question), str(hidden_term)
+        common_difference)
+    hidden_term = choice(get_arithmetic_progression(
+        initial_term,
+        common_difference))
+    question = stringify_progression(progression, hidden_term)
+    return question, str(hidden_term)
